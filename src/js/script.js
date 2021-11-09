59eb61a6fc8db55d83ffdf3b2bc186380ccb5735
SaniTheWay <sanidhyad16@gmail.com>
2021-11-09 23:54:41 +0530

    cahnges

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+      $(".goTop").fadeIn();
+    } else {
+      $(".navbar").removeClass("sticky");
+      $(".goTop").fadeOut();
+    }
+  });
+
+  $(".goTop").click(function () {
+    scroll(0, 0);
+  });
+
+  $(".menu-toggler").click(function () {
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+});
+
+function openProject(evt, skill) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(skill).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " active";
+}
+
+
